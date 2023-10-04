b84f7d794f150e277f945cecc1168bdb7cc89b8d
dwerty3 <106430863+dwerty3@users.noreply.github.com>
2023-10-04 21:01:37 +0700

    Update values.py
    Fix syntax error

--- selfdrive/car/toyota/values.py
+++ selfdrive/car/toyota/values.py
@@ -704,14 +704,14 @@ FW_VERSIONS = {
   },
   CAR.CAMRYH_TSS2: {
     (Ecu.eps, 0x7a1, None): [
-      b'8965B33621\x00\x00\x00\x00\x00\x00', /* <-- notice that the firmware from car params is added here. */
+      b'8965B33621\x00\x00\x00\x00\x00\x00', # notice that the firmware from car params is added here.
       b'8965B33630\x00\x00\x00\x00\x00\x00',
       b'8965B33650\x00\x00\x00\x00\x00\x00',
     ],
     (Ecu.abs, 0x7b0, None): [
       b'F152633D00\x00\x00\x00\x00\x00\x00',
       b'F152633D60\x00\x00\x00\x00\x00\x00',
-      b'F152633214\x00\x00\x00\x00\x00\x00', /* <-- notice that the firmware from car params is added here. */
+      b'F152633214\x00\x00\x00\x00\x00\x00', # notice that the firmware from car params is added here.
       b'F152633310\x00\x00\x00\x00\x00\x00',
     ],
     (Ecu.engine, 0x700, None): [
@@ -719,7 +719,7 @@ FW_VERSIONS = {
       b'\x018966306Q7000\x00\x00\x00\x00',
       b'\x018966306T0000\x00\x00\x00\x00',
       b'\x018966306V1000\x00\x00\x00\x00',
-      b'\x018966306L9000\x00\x00\x00\x00', /* <-- notice that the firmware from car params is added here. */
+      b'\x018966306L9000\x00\x00\x00\x00', #notice that the firmware from car params is added here.
       b'\x01896633T20000\x00\x00\x00\x00',
     ],
     (Ecu.fwdRadar, 0x750, 15): [
@@ -735,7 +735,7 @@ FW_VERSIONS = {
       b'\x028646F3305500\x00\x00\x00\x008646G3304000\x00\x00\x00\x00',
       b'8646F0601400    ',
     ],
-  /* START. notice that the firmware from car params is added here. */
+  # START. notice that the firmware from car params is added here.
     (Ecu.dsu, 0x791, None): [
       b'8821F0609100    ',
     ],
@@ -757,7 +757,7 @@ FW_VERSIONS = {
     (Ecu.epb, 0x750, 44): [
       b'895DF33010\x00\x00\x00\x00\x00\x00',
     ],    
-  /* END. notice that the firmware from car params is added here. */
+  # END. notice that the firmware from car params is added here.
   },
   CAR.CHR: {
     (Ecu.engine, 0x700, None): [
